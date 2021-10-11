45ff2709dfe5aa1eb85296e122ff445f4f799315
bumble2345 <varshithdosapati2000@gmail.com>
2021-10-11 20:55:44 +0530

    Initial commit

---
+++ mytodo.js
@@ -0,0 +1,82 @@
+class todoapp {
+  constructor(todolistelement) {
+    this.todolistelement = todolistelement;
+  }
+  addToDo() {
+    console.log("add clicked");
+    this.todoText = document.querySelector("#todo-text").value;
+    console.log(this.todoText);
+    if (this.todoText == "") {
+      return;
+    } else {
+      this.todoObject = {
+        id: todolist.length + 1,
+        todoText: this.todoText,
+        isDone: false,
+      };
+      console.log(this.todoObject);
+      todolist.unshift(this.todoObject);
+      console.log(this.todolist);
+      //display the added task
+      this.displayTodos();
+    }
+  }
+  displayTodos() {    
+    todolistelements.innerHTML = "";
+    document.querySelector("#todo-text").value = "";
+    todolist.forEach((item) => {
+      this.listElement = document.createElement("li");
+      this.listElement.innerHTML = item.todoText;
+      //console.log(listElement);
+      this.delBtn = document.createElement("i");
+      this.delBtn.setAttribute("class", "fas fa-trash-alt");
+      this.delBtn.setAttribute("data-id", item.id);
+      if (item.isDone) {
+        listElement.setAttribute("class", "checked");
+      }
+      this.listElement.setAttribute("data-id", item.id);
+      //ADD EVENT TO LIST ITEM
+      this.listElement.addEventListener("click", function (e) {
+        this.selectedID = e.target.getAttribute("data-id");
+        main.doneToDo();
+      });
+      this.delBtn.addEventListener("click", function (e) {
+        this.delID = e.target.getAttribute("data-id");
+        main.deleteitem(this.delID);
+      });
+      todolistelements.appendChild(this.listElement);
+      this.listElement.appendChild(this.delBtn);
+    });
+  }
+  doneToDo() {
+    this.selectedIdIndex = todolist.findIndex(
+      (item) => item.id == this.selectedID
+    );
+    console.log(this.selectedIdIndex);
+    if (this.selectedIdIndex == -1) {
+      return;
+    } else {
+      this.todolist[selectedIdIndex].isDone
+        ? (this.todolist[selectedIdIndex].isDone = false)
+        : (this.todolist[selectedIdIndex].isDone = true);
+      console.log(this.todolist[this.selectedIdIndex]);
+      this.displayTodos();
+    }
+  }
+  deleteitem() {
+    this.deleteIndex = todolist.findIndex((item) => item.id == this.delID);
+    todolist.splice(this.deleteIndex, 1);
+    this.displayTodos();
+  }
+
+  
+}
+const todolistelement = document.querySelector("[todo-text]");
+const main = new todoapp(todolistelement);
+const todolistelements = document.querySelector("#to-do-list-ul");
+const todolist = [];
+
+addtask.addEventListener("click", (button) => {
+  main.addToDo();
+  //calculator.updateDisplay()
+});
